44225d2ac710e8ce79a4d912c94ab5aa019bf1ea
Atumah Daniel <atumahdaniel@gmail.com>
2020-06-15 00:02:38 +0100

    Updates

---
+++ precache-manifest.f32601a575ed5d67cb2d2c7ea3392b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c7ef72ca7141b62d0d874971a38d6b",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "8aa70ad9c89cbc220a77",
+    "url": "/contacts-app/static/css/main.3f8a309d.chunk.css"
+  },
+  {
+    "revision": "477332654eb8468d486a",
+    "url": "/contacts-app/static/js/2.99302f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts-app/static/js/2.99302f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa70ad9c89cbc220a77",
+    "url": "/contacts-app/static/js/main.b9b65d88.chunk.js"
+  },
+  {
+    "revision": "09fbad26997f574d3e75",
+    "url": "/contacts-app/static/js/runtime-main.f41eb3ba.js"
+  }
+]);
