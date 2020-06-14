8fe98e1299c506d538a0d0c8309a3c7361dcb94c
Atumah Daniel <atumahdaniel@gmail.com>
2020-06-14 20:15:23 +0100

    failed attempt at implementing a coonter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+      let count = 0;
+
+  const message = "Kill station rulls the underworld";
+
+  const handleClick = () => {
+    count++
+    prompt(`Declare Your Love For Kill Station ${count}`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi there</h1>
+      <h2 onClick={handleClick}>
+        
+        {message}
+      </h2>
     </div>
   );
-}
+};
 
 export default App;
