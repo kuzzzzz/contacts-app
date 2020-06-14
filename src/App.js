7747ca689b64abc6776e74edf17619ff633a2883
Atumah Daniel <atumahdaniel@gmail.com>
2020-06-14 23:19:27 +0100

    simple contact list using

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from "react";
+import "./App.css";
+import ContactCard from "./Contact";
 
 const App = () => {
-      let count = 0;
-
-  const message = "Kill station rulls the underworld";
-
-  const handleClick = () => {
-    count++
-    prompt(`Declare Your Love For Kill Station ${count}`);
-  }
+  const contacts = [
+    {
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5603AQGHHh-O8h0qPQ/profile-displayphoto-shrink_200_200/0?e=1597881600&v=beta&t=xo2BKzaFrAV_1KvMvh6D98WxAY8uCNBwB0rDWJuyP6A",
+      name: "Atumah Daniel",
+      email: "jenny.han@notreal.com",
+      age: 23
+    },
+    {
+      avatar:
+        "https://pbs.twimg.com/profile_images/1265578058947465216/RH7oINIU_400x400.jpg",
+      name: "Ceeynan",
+      email: "iamfabulouse@gmail.com",
+      age: 23
+    },
+    {
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C4D03AQHz37uf3Z-Jxw/profile-displayphoto-shrink_800_800/0?e=1597881600&v=beta&t=VTvxBDiLuY2zEz1AfoxGxNfG_DDWWoI1IQmrWtNBE2Y",
+      name: "Onome Nesimeye",
+      email: "pearlarel@gmail.com",
+      age: 23
+    }
+  ];
   return (
-    <div>
-      <h1>Hi there</h1>
-      <h2 onClick={handleClick}>
-        
-        {message}
-      </h2>
-    </div>
+    <>
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.avatar}
+          name={contact.name}
+          email={contact.email}
+          age={contact.age}
+        />
+      ))}
+    </>
   );
 };
 
